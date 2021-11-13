6d3d80c2262e6bd7c58acee91d77628aa4b6f1e7
zeeshan <mycomputer@Zeeshans-MacBook-Pro.local>
2021-11-13 12:28:37 -0600

    Added started time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
 
+// Moment declaration for day and time 
+moment(Date);
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
+
+var currentTime = moment();
+console.log(currentTime );
+currentTime = currentTime.startOf("hour");
+var beforeTime = moment().startOf('day').add(9, "hours");
+console.log(beforeTime );
